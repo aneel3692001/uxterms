69c21f2256a7073fb7182c39088287e26750a0ad
Aneel <aneel.vanguards@gmail.com>
2025-11-29 01:45:09 +0530

    Updates

---
+++ _next/static/BKcxYeoxebPBygFrGRmjF/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fglossary\u002F[slug]","\u002Fquizzes\u002F[categoryId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ service-worker.js
@@ -0,0 +1,13 @@
+// Basic Service Worker for PWA
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing.');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', (event) => {
+  // Basic pass-through
+  event.respondWith(fetch(event.request));
+});
